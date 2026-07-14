68295a05b4db046d44d3d9ea42f6c913fa238545
Ritikasuneriya <ritikasuneriya04@gmail.com>
2026-07-14 12:05:22 +0530

    first commit

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message":"Welcome to Flask Backend"
+    })
+
+@app.route("/api")
+def api():
+    return jsonify({
+        "name":"Ritika",
+        "course":"DevOps"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
---
+++ frontend/server.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const axios=require("axios");
+
+const app=express();
+
+app.get("/",async(req,res)=>{
+
+    try{
+
+        const response=await axios.get("http://3.95.25.102:5000/api");
+
+        res.send(`
+        <h1>Express Frontend</h1>
+        <h2>Backend Response</h2>
+        <pre>${JSON.stringify(response.data,null,2)}</pre>
+        `);
+
+    }catch(err){
+
+        res.send("Cannot connect backend");
+    }
+
+});
+
+app.listen(3000,"0.0.0.0",()=>{
+
+console.log("Frontend running");
+
+});
